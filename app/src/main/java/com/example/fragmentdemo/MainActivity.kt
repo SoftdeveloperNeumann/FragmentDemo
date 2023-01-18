55b270ea02fc4ae141a6768d33d0376eb200e83a
Frank <f.neumann.berlin@googlemail.com>
2023-01-18 12:24:45 +0100

    Listener für Buttons

--- app/src/main/java/com/example/fragmentdemo/MainActivity.kt
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.fragmentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import com.example.fragmentdemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    lateinit var binding: ActivityMainBinding
+    val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btn1.setOnClickListener(this@MainActivity)
+            btn2.setOnClickListener(this@MainActivity)
+            btn3.setOnClickListener(this@MainActivity)
+            root.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onClick(view: View?) {
+        if(view is Button) {
+            val city = (view as Button).text.toString()
+            Log.d(TAG, "onClick: $city")
+        }else{
+            Log.d(TAG, "onClick: rootview")
+        }
     }
 }
